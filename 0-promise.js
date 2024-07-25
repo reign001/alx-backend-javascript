28e32b710c452e7f945484c10b56ab319d5d4e02
reign001 <olochej543@gmail.com>
2024-07-25 00:22:23 +0000

    my first commit

--- 0-promise.js
+++ 0-promise.js
@@ -1,14 +1,2 @@
 function getResponseFromAPI() {
-    return Promise.resolve("API response data");
-    // or for a rejected promise:
-    // return Promise.reject(new Error("API request failed"));
-}
-
-// Example usage:
-getResponseFromAPI()
-    .then(response => {
-        console.log("API response:", response);
-    })
-    .catch(error => {
-        console.error("API request failed:", error.message);
-    });
+ 	return promise(getResponseFromAPI(Resolve, Reject))};
