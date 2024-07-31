97eb6578486d309a3a623f908f439e92a948a61a
reign001 <olochej543@gmail.com>
2024-07-31 00:52:11 +0000

    correction made

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,17 +1,17 @@
 /**
- * Updates the grades of a list of students in a given city.
- * @param {{
-*   id: Number,
-*   firstName: String,
-*   location: String
-* }[]} students - The list of students.
-* @param {*} city - The city of students.
-* @param {{
-*   studentId: Number,
-*   grade: Number,
-* }[]} newGrades - The new grades to be given to a student.
-* @author Bezaleel Olakunori <https://github.com/B3zaleel>
-* @returns {{id: Number, firstName: String, location: String}[]}
+*Updates the grades of a list of students in a given city.
+*@param {{
+*id: Number,
+*firstName: String,
+*location: String
+*}[]} students - The list of students.
+*@param {*} city - The city of students.
+*@param {{
+*studentId: Number,
+*grade: Number,
+*}[]} newGrades - The new grades to be given to a student.
+*@author Bezaleel Olakunori <https://github.com/B3zaleel>
+*@returns {{id: Number, firstName: String, location: String}[]}
 */
 export default function updateStudentGradeByCity(students, city, newGrades) {
  const defaultGrade = { grade: 'N/A' };
