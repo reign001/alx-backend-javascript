d45f1762ef6be13bd20465be7003411eba755e12
reign001 <olochej543@gmail.com>
2024-07-31 00:43:51 +0000

    corrections

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,12 +1,12 @@
 /**
- * Retrieves the sum of ids of a list of students.
- * @param {{
-*   id: Number,
-*   firstName: String,
-*   location: String
-* }[]} students - The list of students.
-* @author Bezaleel Olakunori <https://github.com/B3zaleel>
-* @returns {Number}
+*Retrieves the sum of ids of a list of students.
+*@param {{
+*id: Number,
+*firstName: String,
+*location: String
+*}[]} students - The list of students.
+*@author Bezaleel Olakunori <https://github.com/B3zaleel>
+*@returns {Number}
 */
 export default function getStudentIdsSum(students) {
  if (students instanceof Array) {
